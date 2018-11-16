c31a28a7140c12896b508c7cd1daeaa254e7c870
Alexandre DILLON <alexdillon1999@gmail.com>
2018-11-16 19:52:39 +0100

    add Serializer

---
+++ lib/Serializer.cc
@@ -0,0 +1,62 @@
+#include "Serializer.h"
+
+Serializer::Serializer(){
+	readPos = 0;
+	writePos = 0;
+};
+
+void Serializer::printData(){
+	for (size_t i = 0; i < writePos; ++i){
+		std::cout << (unsigned)data[i] << '.';
+	}
+	std::cout << std::endl;
+}
+
+size_t Serializer::getReadPos(){
+	return readPos;
+}
+
+size_t Serializer::getWritePos(){
+	return writePos;
+}
+
+size_t Serializer::getSize(){
+	return data.size();
+}
+
+void Serializer::setReadPos(size_t pos){
+	if(data.size()>pos){readPos = pos;}
+}
+
+void Serializer::setWritePos(size_t pos){
+	if(data.size()>pos){writePos = pos;}
+}
+
+void Serializer::clear(){
+	data.clear();
+	setReadPos(0);
+	setWritePos(0);
+}
+
+void Serializer::Serialize(const void* d, size_t Size){
+
+	if((writePos + Size) > getSize()){
+		data.resize(writePos + Size);				
+	}
+
+    for (size_t i = 0; i < Size; ++i) {
+      data[writePos + Size - i -1] = static_cast<uint8_t>( *(char *)d >> (8 * i));
+    }
+
+    writePos += Size;
+}
+
+template<typename T> 
+T Serializer::Deserialize(T type, size_t Size){
+	T x;
+	for (size_t i = 0; i < Size; ++i){
+		x = data[readPos+i] << (8*(Size-i-1));
+	}
+	readPos += Size;
+	return x;
+}
---
+++ lib/Serializer.h
@@ -0,0 +1,36 @@
+#include <iostream> 
+#include <vector>
+
+typedef uint8_t Int;
+
+class Serializer{
+
+	private:
+		std::vector<Int> data;
+		size_t readPos;
+		size_t writePos;
+
+
+	public:
+		Serializer();
+
+		void printData();
+
+		size_t getReadPos();
+
+		size_t getWritePos();
+
+		size_t getSize();
+
+		void setReadPos(size_t pos);
+
+		void setWritePos(size_t pos);
+
+		void clear();
+
+		void Serialize(const void* d, size_t Size);
+
+		template<typename T> 
+		T Deserialize(T type, size_t Size);
+		
+};
