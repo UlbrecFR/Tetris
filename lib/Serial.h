c540952e6b5b2b7a55003a70e189dd6a644c4084
UlbrecFR <felixroyer@laposte.net>
2018-11-28 10:46:56 +0100

    Add messages

--- lib/Serial.h
+++ lib/Serial.h
@@ -1,10 +1,58 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include "Tetromino.h"
 
-const static size_t max_length = 1024;
+const static size_t maxLength = 1024;
 
 typedef struct Message {
-	char msg[max_length];
+	char msg[maxLength];
 	size_t length;
 }Message;
+
+
+typedef struct TetrominoPlaced {
+	Tetromino tetro;
+	int posX;
+	int posY;
+} CTS_TetrominoPlaced;
+
+
+typedef struct ClientConnectionLost {
+	enum {
+		ClientQuit,
+		ClientCrash,
+		ClientDisconnected
+	} ErrorType;
+} CTS_ClientConnectionLost;
+
+
+typedef struct NewTetromino {
+	Tetromino newTetro;
+} STC_NewTetromino;
+
+typedef struct UpdateOtherPlayer {
+	int grid[8];
+} STC_UpdateOtherPlayer;
+
+typedef struct RequestClientToServer{
+	enum {
+		TYPE_TETROMINO_PLACED,
+		TYPE_CLIENT_LOST
+	} Type;
+	union {
+		CTS_TetrominoPlaced tetroMsg;
+		CTS_ClientConnectionLost discoMsg;
+	} Content;
+} Request_CTS;
+
+typedef struct RequestServerToClient{
+	enum {
+		TYPE_NEW_TETROMINO,
+		TYPE_UPDATE_OTHER
+	} Type;
+	union {
+		STC_NewTetromino newTetroMsg;
+		STC_UpdateOtherPlayer updateOtherMsg;
+	} Content;
+} Request_STC;
